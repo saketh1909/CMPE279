520112e485113766cdae9317259aa84c2e70820d
akashamin01 <aakashjamin1239@gmail.com>
2022-04-15 15:39:55 -0700

    Resolved Errors

--- Assignment1/server.c
+++ Assignment1/server.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket, message;
+    int serverFd, newSocket, message;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
@@ -17,13 +17,13 @@ int main(int argc, char const *argv[])
     char *hello = "Hello from server";
 
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("Socket Failed");
         exit(EXIT_FAILURE);
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+    if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
 	&opt, sizeof(opt)))
     {
         perror("setsockopt");
@@ -33,20 +33,20 @@ int main(int argc, char const *argv[])
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
 
-    if (bind(server_fd, (struct sockaddr *)&address,
+    if (bind(serverFd, (struct sockaddr *)&address,
 	sizeof(address)) < 0)
     {
         perror("Bind Failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 3) < 0)
+    if (listen(serverFd, 3) < 0)
     {
         perror("Listen");
         exit(EXIT_FAILURE);
     }
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+    if ((newSocket = accept(serverFd, (struct sockaddr *)&address,
                        (socklen_t*)&addrlen))<0)
     {
         perror("Accept");
@@ -69,9 +69,9 @@ int main(int argc, char const *argv[])
     } 
 
     if(status){
-            message = read(new_socket, buffer, 1024);
+            message = read(newSocket, buffer, 1024);
             printf("Read %d bytes: %s\n", message , buffer);
-            send(new_socket, hello, strlen(hello), 0);
+            send(newSocket, hello, strlen(hello), 0);
             printf("Hello Message Sent\n");
     }
 
